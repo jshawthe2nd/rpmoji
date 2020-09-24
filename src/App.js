ae6b47dbb61479cd131cbc2a103005192d957458
John Shaw <jshawthe2nd@gmail.com>
2020-09-24 14:29:22 -0700

    Adjust margin of title and remove paragraph

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <h1>RPmoJi</h1>
-      <p>RPmoJi (RPG, but made with emojis) is a pet project of mine to keep myself updated with React and thinking about coding while hunting for a job. Coincidentally, it's also meant to be a project to entice potential employers!</p>
+      
       <Game>
           <Party/>
           <Menu />
